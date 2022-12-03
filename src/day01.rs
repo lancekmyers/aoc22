07aa0dc0442a49ea799b26e6237de5479454bb84
lancekmyers <lancekmyers@gmail.com>
2022-12-02 19:52:26 -0600

    switch to binary heap

--- src/day01.rs
+++ src/day01.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-
+use std::collections::{BinaryHeap};
+use std::cmp::Reverse;
 
 pub fn soln(path : &str) -> (i64, i64) {
     let file = File::open(path).unwrap();
@@ -8,72 +9,73 @@ pub fn soln(path : &str) -> (i64, i64) {
 
     let tt = top3_cal(lines);
     
-    let a = tt.max();
+    let a = tt.clone().max();
     let b = tt.sum();
 
     (a, b)
 }
 
-#[derive(Debug)]
-struct Top3 {
-    top3 : [i64;3],
+#[derive(Debug, Clone)]
+struct TopK<const K : usize> {
+    topk : BinaryHeap<Reverse<i64>>,
 }
 
-impl Top3 {
-    fn insert(self, x : i64) -> Self {
-        let a = self.top3[0];
-        let b = self.top3[1];
-        let c = self.top3[2];
-        if (x < a) {
-            self
-        } else if (x < b) {
-            Top3 {top3 : [x, b, c]}
-        } else if (x < c) {
-            Top3 {top3 : [b, x, c]}
-        } else {
-            Top3 {top3 : [b, c, x]}
+impl<const K : usize> TopK<K> {
+    fn insert(&mut self, x : i64) {
+        if self.topk.len() < K {
+            self.topk.push(Reverse(x));
+        }
+        else {
+            match self.topk.peek() {
+                None => 
+                    self.topk.push(Reverse(x)),
+                Some(Reverse(y)) => if (x > *y) {
+                    let _ = self.topk.pop();
+                    self.topk.push(Reverse(x));
+                }
+            }
         }
     }
-    fn sum(&self) -> i64 {
-        self.top3.iter().sum()
+    fn sum(self) -> i64 {
+        self.topk.into_iter().map(|Reverse(x)| x).sum()
     }
-    fn max(&self) -> i64 {
-        self.top3[2]
+    fn max(self) -> i64 {
+        self.topk.into_iter().map(|Reverse(x)| x).max().unwrap_or(0)
     }
     
-    const EMPTY : Self = Top3 {top3: [0, 0, 0]};
+    fn new() -> Self { TopK { topk : BinaryHeap::new() } }
 
 }
 
-fn top3_cal<I>(lines : I) -> Top3 
-    where I : Iterator<Item = String> {
-
-    let (tt, acc) = lines.map(
-        |l| l.parse::<i64>()
-    ).fold((Top3::EMPTY, 0), 
-        |(tt , running_sum), x| match x {
-            Ok(x) => (tt, running_sum + x),
-            Err(_) => (tt.insert(running_sum), 0),
-    });
-    
-    tt.insert(acc)
+fn top3_cal<I>(lines : I) -> TopK<3> where I : Iterator<Item = String> {
+    let mut tt = TopK::<3>::new();
+    let mut acc = 0;
+    for line in lines {
+        let cal = line.parse::<i64>();
+        match cal {
+            Ok(x) => acc += x,
+            Err(_) => {tt.insert(acc); acc = 0}
+        }
+    }
+    tt.insert(acc);
+    tt
 }
 
 
 #[cfg(test)]
 mod tests {
     use crate::day01::{soln};
-    use super::Top3;
+    use super::TopK;
 
     #[test]
     fn bar() {
-        let mut x = Top3::EMPTY;
-        x = x.insert(6);
-        x = x.insert(4);
-        x = x.insert(11);
-        x = x.insert(24);
-        x = x.insert(10);
-        assert_eq!(x.sum(), 45); 
+        let mut x = TopK::<3>::new();
+        x.insert(6);
+        x.insert(4);
+        x.insert(11);
+        x.insert(24);
+        x.insert(10);
+        assert_eq!(x.clone().sum(), 45); 
         assert_eq!(x.max(), 24); 
     }
 
