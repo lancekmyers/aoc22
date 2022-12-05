b72ae75e8695c98792602fa61d14bef7039b4088
lancekmyers <lancekmyers@gmail.com>
2022-12-04 20:50:35 -0600

    day 4 solution

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 pub mod day01;
 pub mod day02;
-
+pub mod day04;
 fn main() {
     let (a, b) = day01::soln("data/day01.txt");
     println!("Day 01");
@@ -10,6 +10,11 @@ fn main() {
     println!("\tPart B: {b}");   
 
     let (a, b) = day02::soln("data/day02.txt");
+    println!("Day 02");
+    println!("\tPart A: {a}");
+    println!("\tPart B: {b}");
+
+    let (a, b) = day04::soln("data/day04.txt");
     println!("Day 02");
     println!("\tPart A: {a}");
     println!("\tPart B: {b}");
